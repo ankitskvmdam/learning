d4b74e47049907c83f9b80547153eeac08d35dbf
Ankit Kumar <ankits.kvmdam@hotmail.com>
2019-02-02 00:52:29 +0530

    C++ hello world add

---
+++ C++/hello.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+int main(void){
+
+    cout << "****************" << endl;
+    cout << "* Hello World! *" << endl;
+    cout << "****************" << endl;
+    
+    return 0;
+}
